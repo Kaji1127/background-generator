1fabb99a109d4f9dae2bd327cdc47e9bb21c8827
K.Kishin <riesgraf.birsack.1226@gmail.com>
2021-03-02 13:54:52 +0900

    ランダムで背景色を生成

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-var css = document.querySelector('h3');
-var color1 = document.querySelector('.color1');
-var color2 = document.querySelector('.color2');
-var body = document.getElementById('gradient');
+/* Variables */
+const css = document.querySelector('h3');
+const color1 = document.querySelector('.color1');
+const color2 = document.querySelector('.color2');
+const body = document.getElementById('gradient');
+const randomBg = document.getElementById('color-random');
 
-var setGradient = function () {
+/* Functions */
+
+// bodyの背景色をinputを使って選ばれた色に変更
+const setGradient = function () {
 	body.style.background = 'linear-gradient(to right, ' + color1.value + ', ' + color2.value + ')';
 };
 
-window.addEventListener('load', function () {
-	color1.value = '#ff0000';
-	color2.value = '#ffff00';
-});
+// inputのvalueをランダムに生成
+const randomBackgroundColor = function () {
+	color1.value = `#${Math.floor(Math.random() * 16 ** 6).toString(16)}`;
+	color2.value = `#${Math.floor(Math.random() * 16 ** 6).toString(16)}`;
+	setGradient();
+};
 
 color1.addEventListener('input', setGradient);
 color2.addEventListener('input', setGradient);
+randomBg.addEventListener('click', randomBackgroundColor);
